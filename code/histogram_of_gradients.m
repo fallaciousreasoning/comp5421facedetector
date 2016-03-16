58d435bf2722fa70c8b0c799821ac754f531dfa5
Jay Harris <jay.harris@outlook.co.nz>
2016-03-16 11:50:46 +0800

    Updates the HOG thing. It still doesn't work. At all

--- code/histogram_of_gradients.m
+++ code/histogram_of_gradients.m
@@ -12,17 +12,64 @@ vertical = imfilter(image, vertical_filter);
 orientations = zeros(image_size);
 magnitudes = zeros(image_size);
 
-for i = 1:image_size(1)
-    for j = 1:image_size(2)
-        dx = horizontal(i, j);
-        dy = vertical(i, j);
+hog_width = floor(image_size(2)/cell_size);
+hog_height = floor(image_size(1)/cell_size);
+pixels_per_cell = 31;
+hog = zeros(hog_width, hog_height, pixels_per_cell);
+
+for i = 1:hog_height - 1
+    for j = 1:hog_width - 1
+        %The buckets for different gradient orientations
+        buckets = zeros(1, pixels_per_cell);
         
-        magnitudes(i, j) = sqrt(dx*dx + dy*dy);
-        orientations(i, j) = atan(dy / dx);
+        start_y = (i - 1) * cell_size + 1;
+        start_x = (j - 1) * cell_size + 1;
+        
+        for k = 1:cell_size
+            for l = 1:cell_size 
+                y = start_y + k;
+                x = start_x + l;
+                
+                dx = horizontal(y, x);
+                dy = vertical(y, x);
+        
+                magnitudes(y, x) = sqrt(dx*dx + dy*dy);
+                orientation = atand(dy/dx);
+                if isnan(orientation)
+                    orientation = 0;
+                end
+                
+                while orientation < 0
+                    orientation = 360 + orientation;
+                end
+                orientations(y, x) = orientation;
+                
+                %Calculate which bucket we should put this gradient in
+                bucket_index = floor(orientation / (360 / size(buckets, 2))) + 1;
+                buckets(1, bucket_index) = buckets(1, bucket_index) + magnitudes(y, x);                
+            end
+        end
+        
+        length_squared = 0;
+        for bucket_index = 1:9
+            length_squared = length_squared + buckets(bucket_index) ^ 2;
+        end
+        
+        length = sqrt(length_squared);
+        %Normalize the buckets vector
+        %buckets = buckets ./ length;
+        hog(i, j,:) = buckets;
     end
 end
-
-hog = [];
-
 end
 
+
+function cell = draw_hog(buckets, cell_size)
+    cell = zeros(cell_size * cell_size);
+    angle = 0;
+    
+    for i = 1:size(buckets)
+        %How the fuck do I draw the lines?
+        angle = angle + 360/size(buckets);
+    end
+end
